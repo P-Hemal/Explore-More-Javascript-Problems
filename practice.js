22c18347ceb4d925cb76c1e64f78b3180ef0534e
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-03-15 14:26:40 +0600

    problem 1

--- practice.js
+++ practice.js
@@ -1,20 +1,28 @@
-const bookCart = [
-    {name : 'book1', pages : 100, quantity : 1},
-    {name : 'book2', pages : 200, quantity : 1},
-    {name : 'book3', pages : 300, quantity : 1},
-]
+// const bookCart = [
+//     {name : 'book1', pages : 100, quantity : 1},
+//     {name : 'book2', pages : 200, quantity : 1},
+//     {name : 'book3', pages : 300, quantity : 1},
+// ]
 
 
 
-function paperRequirements(numbers){
-    let sum = 0;
-    for(let i = 0; i < numbers.length; i++){
-        const number = numbers[i];
-        const totalPages = number.pages * number.quantity;
-        sum = sum + totalPages;
-    }
-    return sum;
+// function paperRequirements(numbers){
+//     let sum = 0;
+//     for(let i = 0; i < numbers.length; i++){
+//         const number = numbers[i];
+//         const totalPages = number.pages * number.quantity;
+//         sum = sum + totalPages;
+//     }
+//     return sum;
+// }
+
+// const actualPages = paperRequirements(bookCart);
+// console.log(actualPages);
+
+// Problem 1 
+
+function cubeNumber(num){
+    var cube = num * num * num;
+    console.log(cube);
 }
-
-const actualPages = paperRequirements(bookCart);
-console.log(actualPages);
+cubeNumber('hemal');
